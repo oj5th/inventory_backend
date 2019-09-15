95d3370626b38f64a38f7c506a84901ffc03e724
OJ <ojfifth@gmail.com>
2019-09-16 04:02:07 +0800

    Add nested attributes behavior

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
   has_many :authors, dependent: :destroy
+  accepts_nested_attributes_for :authors, :allow_destroy => true
   has_many :book_genres, dependent: :destroy
   has_many :genres, through: :book_genres
   scope :find_by_field, ->(field, value){ where("#{field} LIKE '%#{value}%'")}
